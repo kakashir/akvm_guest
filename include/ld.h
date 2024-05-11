873ca1c3a45ec63a1de2a107188c036527c529eb
Yuan Yao <yaoyuan0329os@gmail.com>
2024-05-11 15:56:14 +0800

    LD script: Add __kernel_64_high_{start,end}
    Use __kernel_64_high_{start,end} to track the 64bit kernel
    in virtual memory space part footprint. This is preparation
    for early stage memory b_allocator.
    
    Signed-off-by: Yuan Yao <yaoyuan0329os@gmail.com>

---
+++ include/ld.h
@@ -0,0 +1,15 @@
+#ifndef _INCLUDE_LD_H
+#define _INCLUDE_LD_H
+
+#include "types.h"
+
+extern u8 __kernel_16_start;
+extern u8 __kernel_16_end;
+extern u8 __kernel_32_start;
+extern u8 __kernel_32_end;
+extern u8 __kernel_64_low_start;
+extern u8 __kernel_64_low_end;
+extern u8 __kernel_64_high_start;
+extern u8 __kernel_64_high_end;
+
+#endif
