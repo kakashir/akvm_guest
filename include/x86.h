0f859b2f89067122491b719f8fb884f97c2cb121
Yuan Yao <yaoyuan0329os@gmail.com>
2024-06-01 09:31:11 +0800

    x86: Introduce include/asm/x86.h
    Separate asm and c common definition into
    include/asm/x86.h from include/x86.h
    
    include/x86.h includs C only definition, allow
    add more C definition later without break ASM
    code.
    
    Signed-off-by: Yuan Yao <yaoyuan0329os@gmail.com>

--- include/x86.h
+++ include/x86.h
@@ -1,38 +1,7 @@
 #ifndef __x86_H
 #define __x86_H
 
-#define X86_MSR_PAT 0x277
-#define X86_MSR_PAT_DEF_LOW 0x05010406ULL
-#define X86_MSR_PAT_DEF_HIGH 0x00000007ULL
-
-#define X86_MSR_EFER 0xc0000080
-#define X86_MSR_EFER_LME_BIT 8
-#define X86_MSR_EFER_LME 0x100
-
-#define X86_CR0_PG_BIT 31
-#define X86_CR0_PG 0x80000000
-
-#define X86_CR4_PSE_BIT 4
-#define X86_CR4_PSE 0x10
-#define X86_CR4_PAE_BIT 5
-#define X86_CR4_PAE 0x20
-#define X86_CR4_PGE_BIT 7
-#define X86_CR4_PGE 0x80
-
-#define X86_PAGE_SIZE_1G 0x40000000
-#define X86_PAGE_SIZE_4M 0x400000
-#define X86_PAGE_SIZE 0x1000
-
-#define IDT_TYPE_TRAP 0xf
-#define IDT_TYPE_INTR 0xe
-#define INTR_VECTOR_BEGIN 32
-#define MAX_VECTOR_NUMBER 256
-
-#define GDT_TYPE_DATA 0x2
-#define GDT_TYPE_CODE 0xa
-#define GDT_TYPE_TSS 0x9
-
-#ifndef __ASM__
+#include <asm/x86.h>
 #include "types.h"
 
 struct inter_excep_regs {
@@ -130,5 +99,4 @@ void write_cr(int cr, unsigned long val);
 
 #define sel_to_index(x) ((x) >> 3)
 
-#endif
 #endif
